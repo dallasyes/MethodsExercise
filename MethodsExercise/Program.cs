1a3e88595688e6346e6adc7985bc1e7e46bcd3a8
Dallas Yu <dallas_yu@hotmail.com>
2025-06-18 16:54:09 -0500

    Completed C# story exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+            Console.WriteLine($"{name} had a dream that a {color} {animal} was singing along with {band}");
+            
         }
     }
 }
